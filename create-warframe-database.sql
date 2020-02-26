75870501778aa2833e06860cdd2233568228c95d
Derek Yung-Sheng Lin <dyl14@sfu.ca>
2020-02-25 20:45:16 -0800

    completed tables for api_queries, users and reviews

--- create-warframe-database.sql
+++ create-warframe-database.sql
@@ -5,13 +5,28 @@ USE steam;
 SET NAMES utf8mb4;
 SET character_set_client = utf8mb4;
 
-CREATE TABLE review_query (
+CREATE TABLE api_queries (
 	query_number SMALLINT NOT NULL AUTO_INCREMENT,
 	query_cursor VARCHAR(30) NOT NULL,
-	success TINYINT NOT NULL,		
-	number_reviews TINYINT NOT NULL,
+	query_success TINYINT NOT NULL,		
+	number_of_reviews_queried TINYINT NOT NULL,
     query_date date NOT NULL,
     PRIMARY KEY (query_number)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- refer to https://developer.valvesoftware.com/wiki/SteamID for steamID format
+CREATE TABLE users (
+	steam_id VARCHAR(20) NOT NULL,
+    total_playtime SMALLINT NOT NULL,
+    playtime_last_two_weeks SMALLINT NOT NULL,
+	playtime_at_review SMALLINT NOT NULL,
+    last_played date NOT NULL,
+    PRIMARY KEY (steam_id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE reviews (
+	steam_id VARCHAR(20) NOT NULL,
+    user_review TEXT NOT NULL,
+    timestamp_updated INT NOT NULL,
+    recommends_game TINYINT NOT NULL,
+    PRIMARY KEY (steam_id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
