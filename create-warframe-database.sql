102df0cb34754a6f0dc747d57f3fddabe6e9a5f6
Derek Yung-Sheng Lin <dyl14@sfu.ca>
2020-02-26 20:08:34 -0800

    loaded first batch of json data

--- create-warframe-database.sql
+++ create-warframe-database.sql
@@ -1,6 +1,6 @@
-DROP DATABASE IF EXISTS steam;
-CREATE DATABASE steam;
-USE steam;
+DROP DATABASE IF EXISTS warframe;
+CREATE DATABASE warframe;
+USE warframe;
 
 SET NAMES utf8mb4;
 SET character_set_client = utf8mb4;
@@ -8,20 +8,20 @@ SET character_set_client = utf8mb4;
 CREATE TABLE api_queries (
 	query_number SMALLINT NOT NULL AUTO_INCREMENT,
 	query_cursor VARCHAR(30) NOT NULL,
-	query_success TINYINT NOT NULL,		
-	number_of_reviews_queried TINYINT NOT NULL,
-    query_date date NOT NULL,
+	query_success TINYINT,		
+	number_of_reviews_queried TINYINT,
     PRIMARY KEY (query_number)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+INSERT INTO api_queries(query_cursor) VALUES ('*');
 
 CREATE TABLE users (
 	steam_id VARCHAR(20) NOT NULL,
-    total_playtime SMALLINT NOT NULL,
+    total_playtime MEDIUMINT NOT NULL,
     playtime_last_two_weeks SMALLINT NOT NULL,
-	playtime_at_review SMALLINT NOT NULL,
+	playtime_at_review MEDIUMINT NOT NULL,
     last_played date NOT NULL,
     PRIMARY KEY (steam_id)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE reviews (
 	steam_id VARCHAR(20) NOT NULL,
@@ -29,4 +29,4 @@ CREATE TABLE reviews (
     timestamp_updated INT NOT NULL,
     recommends_game TINYINT NOT NULL,
     PRIMARY KEY (steam_id)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
