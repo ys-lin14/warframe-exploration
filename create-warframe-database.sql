27c96453d185a3c8010ec55387515357c5ae2890
Derek Yung-Sheng Lin <dyl14@sfu.ca>
2020-02-27 22:52:54 -0800

    complete data collection

--- create-warframe-database.sql
+++ create-warframe-database.sql
@@ -8,23 +8,22 @@ SET character_set_client = utf8mb4;
 CREATE TABLE api_queries (
 	query_number SMALLINT NOT NULL AUTO_INCREMENT,
 	query_cursor VARCHAR(30) NOT NULL,
-	query_success TINYINT,		
-	number_of_reviews_queried TINYINT,
+	success TINYINT,		
+	reviews_retrieved TINYINT,
     PRIMARY KEY (query_number)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 INSERT INTO api_queries(query_cursor) VALUES ('*');
 
 CREATE TABLE users (
-	steam_id VARCHAR(20) NOT NULL,
+	steam_id VARCHAR(30) NOT NULL,
     total_playtime MEDIUMINT NOT NULL,
     playtime_last_two_weeks SMALLINT NOT NULL,
-	playtime_at_review MEDIUMINT NOT NULL,
-    last_played date NOT NULL,
+    last_played INT NOT NULL,
     PRIMARY KEY (steam_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE reviews (
-	steam_id VARCHAR(20) NOT NULL,
+	steam_id VARCHAR(30) NOT NULL,
     user_review TEXT NOT NULL,
     timestamp_updated INT NOT NULL,
     recommends_game TINYINT NOT NULL,
